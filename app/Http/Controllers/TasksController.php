b37058c4bec1216d127aae71d3e34fc2a67be540
yoshiotachibana <ytachibana@mx5.canvas.ne.jp>
2017-10-14 18:26:12 +0900

    destory syusei

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -117,6 +117,9 @@ class TasksController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $task = Task::find($id);
+        $task->delete();
+        
+        return redirect('/');
     }
 }
